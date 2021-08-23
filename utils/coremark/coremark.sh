a078b8d4b08ab0d98d75314ef0161b5df30b0443
boos4721 <3.1415926535boos@gmail.com>
2021-08-23 15:35:26 +0800

    coremark: fix typo
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- utils/coremark/coremark.sh
+++ utils/coremark/coremark.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo -e "\nRunning CoreMark test,please wait ..."
 /bin/coremark > /tmp/coremark.log
@@ -6,7 +6,7 @@ echo -e "\nRunning CoreMark test,please wait ..."
 Scores=$(cat /tmp/coremark.log 2> /dev/null | grep "CoreMark 1.0" | awk '{print $4}')
 Processer=$(awk -F ':[ ]' '/model name/{printf ($2);exit}' /proc/cpuinfo)
 Time=$(grep "Total time" /tmp/coremark.log | awk '{print $4}')
-[[ -z "${Processer}" ]] && Processer=Unknown
+[[ -z "${Processer}" ]] && Processer=ARMv8
 
 echo -e "\nProcesser: ${Processer}\nScore: ${Scores}\nTime costs: ${Time}"
 echo "(CPU Mark: ${Scores} Scores)" > /etc/bench.log
